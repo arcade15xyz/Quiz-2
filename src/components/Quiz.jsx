b7dd97500cf28a7ff324c14cd635a8ce319f5980
arcade15xyz <90238231+arcade15xyz@users.noreply.github.com>
2025-01-03 17:04:33 +0530

    Deriving value , Outputting Questions, and Registering answers

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import QUESTION from '../questions'
+
+
+export default function Quiz(){
+
+    const [userAnswers, setUserAnswers] = useState([]);
+    const activeQuestionIndex = userAnswers.length;
+
+    function handleSelectAnswer(selectedAnswer){
+        setUserAnswers((prevUserAnswers)=>{
+            return(
+                [...prevUserAnswers,selectedAnswer]
+            );
+        });
+    }
+    return (
+        <div id="question">
+            <h2>{QUESTION[activeQuestionIndex].text}</h2>
+            <ul id="answers">
+                {QUESTION[activeQuestionIndex].answers.map(answer =>(
+                    <li key={answer} className="answer">
+                        <button 
+                        onClick={()=>handleSelectAnswer(answer)}>{answer}</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
