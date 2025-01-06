42a2c0c365c70b484b736cb4a983b81b73a21535
arcade15xyz <90238231+arcade15xyz@users.noreply.github.com>
2025-01-06 09:52:16 +0530

    Shuffling answers and adding Quiz logic

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
-import QUESTION from '../questions'
+import QUESTION from '../questions';
+import quizCompleteImg from '../assets/quiz-complete.png';
 
 
 export default function Quiz(){
 
     const [userAnswers, setUserAnswers] = useState([]);
     const activeQuestionIndex = userAnswers.length;
+    const quizIsComplete = activeQuestionIndex === QUESTION.length;
 
-    const shuffledAnswers = [...QUESTION[activeQuestionIndex].answers];
-    shuffledAnswers.sort(()=>Math.random() - 0.5);
+
 
     function handleSelectAnswer(selectedAnswer){
         setUserAnswers((prevUserAnswers)=>{
@@ -17,7 +18,20 @@ export default function Quiz(){
             );
         });
     }
+
+    if(quizIsComplete){
+        return(
+            <div id="summary">
+                <img src={quizCompleteImg} alt="Trophy icon"/>
+                <h2>Quiz Completed !</h2>
+            </div>
+        )
+    }
+
+    const shuffledAnswers = [...QUESTION[activeQuestionIndex].answers];
+    shuffledAnswers.sort(()=>Math.random() - 0.5);
     return (
+        <div id="quiz">
         <div id="question">
             <h2>{QUESTION[activeQuestionIndex].text}</h2>
             <ul id="answers">
@@ -28,6 +42,7 @@ export default function Quiz(){
                     </li>
                 ))}
             </ul>
+        </div>
         </div>
     )
 }
