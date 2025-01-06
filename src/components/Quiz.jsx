effd6e8c721e70b0f79731d17ed54f293d8ace7e
arcade15xyz <90238231+arcade15xyz@users.noreply.github.com>
2025-01-06 12:38:49 +0530

    For shuffling we used <Answer/> component so that shuffling doesnt happent regularly. we use key with value of {activeQuestionIndex} which will create problem as its sibling component aslo uses same key which is wrong for React .
    splitting components up to solve problem like multi <progress/> it is done coz of 2 sibling components with same key and we solved it making a component of these 2 components and using key on it so that it can be destroyed and re-created angain on key change but my code is still not correct

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,13 +1,16 @@
 import { useState ,useCallback } from "react";
 import QUESTION from '../questions';
 import quizCompleteImg from '../assets/quiz-complete.png';
-import QuestionTimer from "./QuestionTimer";
+import Question from "./Question"; 
+
 
 export default function Quiz(){
 
     const [answerState, setAnswerState] = useState('');
     const [userAnswers, setUserAnswers] = useState([]);
+
     const activeQuestionIndex = answerState === '' ? userAnswers.length : userAnswers.length -1;
+
     const quizIsComplete = activeQuestionIndex === QUESTION.length;
 
 
@@ -42,40 +45,16 @@ export default function Quiz(){
         )
     }
 
-    const shuffledAnswers = [...QUESTION[activeQuestionIndex].answers];
-    shuffledAnswers.sort(()=>Math.random() - 0.5);
     return (
         <div id="quiz">
-        <div id="question">
-            <QuestionTimer 
-            key={activeQuestionIndex}
-            timeout={10000} 
-            onTimeout={handleSkipAnswer}
-             />
-            <h2>{QUESTION[activeQuestionIndex].text}</h2>
-            <ul id="answers">
-                {shuffledAnswers.map(answer =>{
-                    const isSelected = userAnswers[userAnswers.length-1]===answer;
-                    let cssClass = '';
-                    if(answerState === 'answered' && isSelected){
-                        cssClass = 'selected';
-                    }
-                    if((answerState === 'correct' || answerState ==='wrong') && isSelected){
-                        cssClass = answerState;
-                    }
-                    return (                  
-                    <li key={answer} className="answer">
-                        <button 
-                        onClick={()=>handleSelectAnswer(answer)}
-                        className={cssClass}
-                        >{answer}</button>
-                    </li>
-                    )
-                }
-
-                )}
-            </ul>
-        </div>
+            <Question
+                key={activeQuestionIndex}
+                questionText = {QUESTION[activeQuestionIndex].text}
+                answers = {QUESTION[activeQuestionIndex].answers}
+                onSelectAnswer = {handleSelectAnswer}
+                onSkipAnswer = {handleSkipAnswer}
+                answerState={answerState}
+            />
         </div>
     )
 }
