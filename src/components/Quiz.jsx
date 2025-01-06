7d4c8ce5004baad017a6a3519b2cedeeed9e9144
arcade15xyz <90238231+arcade15xyz@users.noreply.github.com>
2025-01-06 11:29:05 +0530

    highlight answers and managing States But on clicking button the answers get swaped which we don't want as re-rendering is happening

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,22 +5,31 @@ import QuestionTimer from "./QuestionTimer";
 
 export default function Quiz(){
 
+    const [answerState, setAnswerState] = useState('');
     const [userAnswers, setUserAnswers] = useState([]);
-    const activeQuestionIndex = userAnswers.length;
+    const activeQuestionIndex = answerState === '' ? userAnswers.length : userAnswers.length -1;
     const quizIsComplete = activeQuestionIndex === QUESTION.length;
 
 
 
    const handleSelectAnswer = useCallback( function handleSelectAnswer(selectedAnswer){
+        setAnswerState('answered');
         setUserAnswers((prevUserAnswers)=>{
-            console.log(
-                'Handle select answer is running ' + activeQuestionIndex
-            )
+            console.log('Handle select answer is running '+activeQuestionIndex);
             return(
                 [...prevUserAnswers,selectedAnswer]
             );
         });
-    },[]);
+        setTimeout(()=>{
+            if(selectedAnswer === QUESTION[activeQuestionIndex].answers[0]){
+                setAnswerState('correct');
+            }
+            else{setAnswerState('wrong');}
+            setTimeout(()=>{
+                setAnswerState('');
+            },2000)
+        },1000)
+    },[activeQuestionIndex]);
 
     const handleSkipAnswer = useCallback(()=>handleSelectAnswer(null), [handleSelectAnswer]);
 
@@ -45,12 +54,26 @@ export default function Quiz(){
              />
             <h2>{QUESTION[activeQuestionIndex].text}</h2>
             <ul id="answers">
-                {shuffledAnswers.map(answer =>(
+                {shuffledAnswers.map(answer =>{
+                    const isSelected = userAnswers[userAnswers.length-1]===answer;
+                    let cssClass = '';
+                    if(answerState === 'answered' && isSelected){
+                        cssClass = 'selected';
+                    }
+                    if((answerState === 'correct' || answerState ==='wrong') && isSelected){
+                        cssClass = answerState;
+                    }
+                    return (                  
                     <li key={answer} className="answer">
                         <button 
-                        onClick={()=>handleSelectAnswer(answer)}>{answer}</button>
+                        onClick={()=>handleSelectAnswer(answer)}
+                        className={cssClass}
+                        >{answer}</button>
                     </li>
-                ))}
+                    )
+                }
+
+                )}
             </ul>
         </div>
         </div>
